b91255a1f3d48497d1811ff62c40925cf0038f3a
seongRyoung <tjdfyd6619@gmail.com>
2024-05-29 17:19:03 +0900

    setting: native초기 세팅

--- jopstice/App.tsx
+++ jopstice/App.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar></StatusBar>
+      <Text>Hello, TypeScript!</Text>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {

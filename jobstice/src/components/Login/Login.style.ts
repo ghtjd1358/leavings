a3ffb3d6716587307a78c9f83627505e7f4699d9
seongRyoung <tjdfyd6619@gmail.com>
2024-05-29 22:17:36 +0900

    feat/loginPage, settingPage

---
+++ jobstice/src/components/Login/Login.style.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+const Styles = StyleSheet.create({
+  logoContainer: {
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+
+  logo: {
+    marginBottom: '10%',
+  },
+
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 12,
+  },
+
+  button: {
+    width: '80%',
+    height: 40,
+    backgroundColor: '#84A6FF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+  },
+});
+
+export default Styles;

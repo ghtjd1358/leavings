42dd25330b107f78f3586ea08f14c422e43dca1c
seongRyoung <tjdfyd6619@gmail.com>
2024-05-29 22:48:32 +0900

    feat:logpage

--- jobstice/src/pages/LoginPage.tsx
+++ jobstice/src/pages/LoginPage.tsx
@@ -1,9 +1,13 @@
 import Login from '../components/Login/Login';
+import { SignUp, FindID, FindPassword } from '../components/Login';
 
 function LoginPage() {
   return (
     <>
       <Login></Login>
+      <SignUp></SignUp>
+      <FindID></FindID>
+      <FindPassword></FindPassword>
     </>
   );
 }

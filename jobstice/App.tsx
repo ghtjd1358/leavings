d1ac311e51377717ab894cd5cb0cf442cf62dc78
nwejin <147528674+nwejin@users.noreply.github.com>
2024-05-30 19:11:13 +0900

    메인 nav

--- jobstice/App.tsx
+++ jobstice/App.tsx
@@ -3,34 +3,35 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, TextInput } from 'react-native';
 import LoginPage from './src/pages/LoginPage';
 import Test from './src/components/DBTest/Test';
+import MainPage from './src/pages/MainPage';
 
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const App: React.FC = () => {
-
   const Stack = createStackNavigator();
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <LoginPage></LoginPage>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='LoginPage'>
-           <Stack.Screen name="Login" component={LoginPage}></Stack.Screen>
-          <Stack.Screen name="Test" component={Test}></Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    // <View style={styles.container}>
+    // {/* <StatusBar style="auto" /> */}
+    //   {/* <LoginPage></LoginPage> */}
+
+    // {/* </View> */}
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Main" component={MainPage} />
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Test" component={Test} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 16,

87fac543eca9ff159afc0bced0f4a250709143bd
nwejin <147528674+nwejin@users.noreply.github.com>
2024-06-04 13:45:20 +0900

    zustand 적용

--- jobstice/src/components/DBTest/Test.tsx
+++ jobstice/src/components/DBTest/Test.tsx
@@ -6,9 +6,11 @@ import {
   TouchableOpacity,
   Image,
   StyleSheet,
+  Button,
 } from 'react-native';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import useNumberBaseStore from '../../../store/test';
 
 const Test: React.FC = () => {
   // DB 연동 테스트를 위한 함수
@@ -17,7 +19,7 @@ const Test: React.FC = () => {
   const dataPost = async (data: string) => {
     try {
       const res = await axios.post(
-        'http://localhost:8081',
+        'http://192.168.1.164:8081',
         { content: data },
         {
           headers: {
@@ -42,6 +44,18 @@ const Test: React.FC = () => {
       console.log(err);
     }
   };
+
+  // zustand
+  const {
+    numberA,
+    numberB,
+    increaseNumberA,
+    increaseNumberB,
+    testText,
+    setData,
+  } = useNumberBaseStore();
+
+  const [isText, setIsText] = useState('');
   return (
     <>
       <StatusBar style="auto" />
@@ -55,6 +69,33 @@ const Test: React.FC = () => {
           onSubmitEditing={testSubmit}
           style={styles.input}
         />
+        <View>
+          <Text>number: {numberA}</Text>
+          <Text>number: {numberB}</Text>
+
+          <Text onPress={increaseNumberA}>A 증가</Text>
+
+          <Text onPress={() => increaseNumberB(3)}>B 증가</Text>
+        </View>
+
+        <TextInput
+          style={styles.input}
+          onChangeText={(e) => {
+            setIsText(e);
+          }}
+          placeholder="zustand 실험"
+          value={isText}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            console.log('Text pressed');
+            setData(isText);
+          }}
+        >
+          <Text>제출</Text>
+        </TouchableOpacity>
+
+        <Text>{testText}</Text>
       </View>
     </>
   );
@@ -62,15 +103,15 @@ const Test: React.FC = () => {
 
 const styles = StyleSheet.create({
   test: {
-    flexDirection: 'row',
+    flexDirection: 'column',
   },
   input: {
     flex: 1,
     backgroundColor: 'white',
     borderColor: 'black',
     borderWidth: 1,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    paddingVertical: 20,
+    paddingHorizontal: 30,
     borderRadius: 10,
     fontSize: 18,
     marginVertical: 20,

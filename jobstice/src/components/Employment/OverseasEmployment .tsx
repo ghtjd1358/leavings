3212f93f3d4f13f1a9fff5f474d05b0f076cb2a8
ghtjd1358 <hojjang18@naver.com>
2024-06-10 17:04:14 +0900

    해외

--- jobstice/src/components/Employment/OverseasEmployment .tsx
+++ jobstice/src/components/Employment/OverseasEmployment .tsx
@@ -1,19 +1,71 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useEffect } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import {employCardNormal, employCardRecommend} from './EmploymentStyle'
+
 
 const OverseasEmployment  = () => {
+  const fetchUrl = async () => {
+    const targetUrl = 'http://localhost:3000/job-search';
+
+    try {
+      const response = await fetch(targetUrl, {
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text>해외에 관한 내용입니다만</Text>
-    </View>
+    <>
+  <ScrollView 
+    showsHorizontalScrollIndicator = {false}
+    style={employCardNormal.normalContainer}>
+    {/* 추천 */}
+    <ScrollView 
+      pagingEnabled
+      horizontal
+      showsHorizontalScrollIndicator = {false}
+      style={employCardRecommend.recommendContainer}>
+      <View style={employCardRecommend.recommendCard}>
+        <Text style={employCardRecommend.recommendCardTitle}>카드 제목</Text>
+        <Text style={employCardRecommend.recommendCardContent}>카드 내용입니 다. 여기에 필요한 정보를 채워 넣으세요.
+        </Text>
+      </View>
+
+      <View style={employCardNormal.normalCard}>
+        <Text style={employCardNormal.normalCardTitle}>카드 제목</Text>
+        <Text style={employCardNormal.normalCardContent}>카드 내용입니다. 여기에 필요한 정보를 채워 넣으세요.</Text>
+      </View>
+    </ScrollView>
+
+    {/* 일반 */}
+      <View style={employCardNormal.normalCard}>
+        <Text style={employCardNormal.normalCardTitle}>카드 제목</Text>
+        <Text style={employCardNormal.normalCardContent}>카드 내용입니다. 여기에 필요한 정보를 채워 넣으세요.</Text>
+      </View>
+
+      <View style={employCardNormal.normalCard}>
+        <Text style={employCardNormal.normalCardTitle}>카드 제목</Text>
+        <Text style={employCardNormal.normalCardContent}>카드 내용입니다. 여기에 필요한 정보를 채워 넣으세요.</Text>
+      </View>
+    </ScrollView>
+    </>    
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
-
-export default OverseasEmployment  
+export default OverseasEmployment ;
